fedff4751bcf935a31a5d8e2dd34ed72683ad2a7
zakorra <svenp666@gmail.com>
2018-07-05 18:50:07 +0200

    dropdown

---
+++ Assets/_Scripts/ui/settings/CreateSettingsManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using _Scripts.entities;
+
+public class CreateSettingsManager : MonoBehaviour {
+
+	public Dropdown dropdownGalaxySize;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		List<Dropdown.OptionData> galaxySizeTypes = Enum.GetValues(typeof(GalaxySizeType))
+			.Cast<GalaxySizeType>()
+			.Select(v => new Dropdown.OptionData(v.ToString()))
+			.ToList();
+
+		Dropdown.OptionData a;
+		dropdownGalaxySize.options = galaxySizeTypes;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
